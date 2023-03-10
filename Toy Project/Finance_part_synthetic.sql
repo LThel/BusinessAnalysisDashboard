95409b06a13db94b5412ac0e2d33949d7a4ad699
LThel <77852026+LThel@users.noreply.github.com>
2023-03-10 14:52:43 +0100

    Update Finance_part_synthetic.sql

--- Toy Project/Finance_part_synthetic.sql
+++ Toy Project/Finance_part_synthetic.sql
@@ -1,6 +1,6 @@
 use toys_and_models;
 
--- Number of Command and the total ammount due per customerNumber
+-- Q1-Number of Command and the total ammount due per customerNumber
 select c.country, # select the country because we want to see the amount per country
 sum(od.quantityOrdered*od.priceEach) as amount_due, # calculate the amount (quantityOrdered * priceEach) to every orderLineNumber and then sum it (the idea is then to group it per country)
 count(od.orderNumber) as Number_of_orders  #count the number of orders (the idea is then to group it per country)
@@ -11,7 +11,7 @@ where o.orderDate >=  (NOW() - INTERVAL 2 MONTH)  # I want only the last two mon
 group by c.country; # I want to get the information per country
 
 
--- To get the customers that still have some stuff to pay 
+-- Q2-To get the customers that still have some stuff to pay 
 -- I am still wroking on this place to get a proper way than distinct to get this table and above all to try to link the payments with the orderNumber and not the customerNumber
 
 select c.customerNumber, count(distinct(o.orderNumber)) as Number_of_Command, sum(distinct(od.priceEach*od.quantityOrdered)) as total_amount_due, sum(distinct(p.amount)) as amount_paid, ((sum(distinct(od.priceEach*od.quantityOrdered))) - (sum(distinct(p.amount)))) as Still_have_to_be_paid
