61abe60712b23615b631749a73a11b541461c78c
LThel <77852026+LThel@users.noreply.github.com>
2023-03-11 00:18:18 +0000

    Add files via upload

--- Toy Project/Finance_part_synthetic.sql
+++ Toy Project/Finance_part_synthetic.sql
@@ -1,6 +1,6 @@
 use toys_and_models;
 
--- Q1-Number of Command and the total ammount due per customerNumber
+-- Finance 1 - Number of Command and the total ammount due per customerNumber
 select c.country, # select the country because we want to see the amount per country
 sum(od.quantityOrdered*od.priceEach) as amount_due, # calculate the amount (quantityOrdered * priceEach) to every orderLineNumber and then sum it (the idea is then to group it per country)
 count(od.orderNumber) as Number_of_orders  #count the number of orders (the idea is then to group it per country)
@@ -11,21 +11,16 @@ where o.orderDate >=  (NOW() - INTERVAL 2 MONTH)  # I want only the last two mon
 group by c.country; # I want to get the information per country
 
 
--- Q2-To get the customers that still have some stuff to pay 
--- I am still wroking on this place to get a proper way than distinct to get this table and above all to try to link the payments with the orderNumber and not the customerNumber
+-- Finance 2 - To get the customers that still have some stuff to pay 
 
-select c.customerNumber, count(distinct(o.orderNumber)) as Number_of_Command, sum(distinct(od.priceEach*od.quantityOrdered)) as total_amount_due, sum(distinct(p.amount)) as amount_paid, ((sum(distinct(od.priceEach*od.quantityOrdered))) - (sum(distinct(p.amount)))) as Still_have_to_be_paid
-from customers as c 
-inner join orders as o on c.customerNumber = o.customerNumber
+# I use a with to create a temporary table to be able to put a where clause at the end
+with Finance_per_customer as (select o.customerNumber, sum(od.quantityOrdered*od.priceEach) as Amount_that_have_to_be_paid, n.already_paid, (sum(od.quantityOrdered*od.priceEach) - n.already_paid) as Still_have_to_be_paid
+from orders as o
 inner join orderdetails as od on o.orderNumber = od.orderNumber
-inner join payments as p on c.customerNumber = p.customerNumber
-group by c.customerNumber;
+inner join (select customerNumber, sum(amount) as already_paid
+from payments group by customerNumber) as n on n.customerNumber = o.customerNumber #I did here a subquery to avoid the pollution by the differents paymentcheck and paymentDate
+group by o.customerNumber)
+Select * from Finance_per_customer as Fpc where Fpc.Still_have_to_be_paid <> 0; #Here is the where clause to only get the customers that still need to pay something
 
-
-select * -- o.customerNumber, sum(od.quantityOrdered*od.priceEach) as amount_due, sum(p.amount)
-from orderdetails as od
-inner join orders as o on o.orderNumber = od.orderNumber
-inner join payments as p on o.customerNumber = p.customerNumber ;
--- group by o.customerNumber;
-
-select customerNumber, sum(amount) from payments group by customerNumber;
+#Here are the customerNumber which are not linked to any payment and to any order. I didn't find any explication about them yet.
+select orders.orderNumber, orders.customerNumber, orders.status from orders where orders.orderNumber in (select customerNumber from customers where not exists (SELECT checkNumber FROM payments where payments.customerNumber = customers.customerNumber));
