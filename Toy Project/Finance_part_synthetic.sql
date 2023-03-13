0422497b89ece6e89238f2010abb08b19b6db37b
LThel <77852026+LThel@users.noreply.github.com>
2023-03-13 14:20:18 +0000

    Addition of the percentage of credit_limit already reached regarding the amount that is still needed to be paid.

--- Toy Project/Finance_part_synthetic.sql
+++ Toy Project/Finance_part_synthetic.sql
@@ -14,13 +14,17 @@ group by c.country; # I want to get the information per country
 -- Finance 2 - To get the customers that still have some stuff to pay 
 
 # I use a with to create a temporary table to be able to put a where clause at the end
-with Finance_per_customer as (select o.customerNumber, sum(od.quantityOrdered*od.priceEach) as Amount_that_have_to_be_paid, n.already_paid, (sum(od.quantityOrdered*od.priceEach) - n.already_paid) as Still_have_to_be_paid
+with Finance_per_customer as (select o.customerNumber, c.phone, sum(od.quantityOrdered*od.priceEach) as Amount_that_have_to_be_paid, n.already_paid, (sum(od.quantityOrdered*od.priceEach) - n.already_paid) as Still_have_to_be_paid, round((((sum(od.quantityOrdered*od.priceEach) - n.already_paid)*100)/c.creditLimit)) as Proportion_of_credit_allows_already_reached, c.creditLimit
 from orders as o
 inner join orderdetails as od on o.orderNumber = od.orderNumber
+inner join customers as c on o.customerNumber = c.customerNumber
 inner join (select customerNumber, sum(amount) as already_paid
 from payments group by customerNumber) as n on n.customerNumber = o.customerNumber #I did here a subquery to avoid the pollution by the differents paymentcheck and paymentDate
 group by o.customerNumber)
 Select * from Finance_per_customer as Fpc where Fpc.Still_have_to_be_paid <> 0; #Here is the where clause to only get the customers that still need to pay something
 
 #Here are the customerNumber which are not linked to any payment and to any order. I didn't find any explication about them yet.
-select orders.orderNumber, orders.customerNumber, orders.status from orders where orders.orderNumber in (select customerNumber from customers where not exists (SELECT checkNumber FROM payments where payments.customerNumber = customers.customerNumber));
+# Here the customers that are not linked to any paymentpostalCode
+select customerNumber from customers where not exists (SELECT checkNumber FROM payments where payments.customerNumber = customers.customerNumber); 
+#But then, if you look for any command from them, you can't find anything.
+select orders.orderNumber, orders.customerNumber, orders.status from orders where orders.customerNumber in (select customerNumber from customers where not exists (SELECT checkNumber FROM payments where payments.customerNumber = customers.customerNumber));
