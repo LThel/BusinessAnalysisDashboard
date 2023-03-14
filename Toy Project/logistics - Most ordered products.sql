ccdf9b8af13420474c1773319f992b0ce2115d20
LThel <77852026+LThel@users.noreply.github.com>
2023-03-14 19:44:32 +0100

    I add the average quantity ordered per months for the top 5 items to have a more precise idea about how long can last the stock left

--- Toy Project/logistics - Most ordered products.sql
+++ Toy Project/logistics - Most ordered products.sql
@@ -10,7 +10,19 @@ ORDER BY Total_Quantity desc
 LIMIT 5;
 
 
+-- I did this to add the idea I told you yesterday (about having the average sales per months for the top 5 items to have a more precise idea about the stock left)
 
+# I use subqueries as we saw today to build it => The action of the subquery is explain after the second bracket (I mean the bracket which the subquery)wich will appears in yellow if you dowload the file
+
+select p.productCode, AvgPerMonth.Total_Quantity_Ordered, p.quantityInStock,  AvgPerMonth.average_quantity_orders_by_month as Average_quantity_orders_by_month, quantityInStock/AvgPerMonth.average_quantity_orders_by_month as How_many_months_left_we_have -- The columns that I want to see in my result table
+from (select od.productCode, (sum(od.quantityOrdered)/(count(distinct(month(o.orderDate)))*count(distinct(year(o.orderDate))))) as Average_quantity_orders_by_month, sum(od.quantityOrdered) as Total_Quantity_Ordered
+from orders as o inner join orderdetails as od on o.orderNumber = od.orderNumber 
+where od.productCode in (select odbis.productCode 
+from (select od.productCode, sum(od.quantityOrdered) from orderdetails as od group by productCode order by sum(od.quantityOrdered) desc limit 5) -- Here, I get the top 5 most ordered products
+as odbis) -- Here, I select the productCode of the top 5 most ordered products
+group by productCode) as AvgPerMonth -- Here, I have the productCode of the top 5 and the monthly average number of orders for each product of the top 5
+inner join products as p on p.productCode = AvgPerMonth.productCode -- Here I join the productCode to be able to display it in my result table
+order by Total_Quantity_Ordered desc;
 
 
 
