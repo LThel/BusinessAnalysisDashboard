0861da8e7faef30c443b285ddb6ffd48d829c629
LuPissarra <lpissarra@gmail.com>
2023-03-15 09:08:28 +0000

    Query half ready

--- Toy Project/ToyHRquery.sql
+++ Toy Project/ToyHRquery.sql
@@ -1,13 +1,26 @@
 #HR KPI:Monthly, the 2 sellers with the highest turnover
-#just checking for changes
-select employeeNumber, firstName from employees order by employeeNumber desc;
-select customerNumber, salesRepEmployeeNumber from customers 
-where salesRepEmployeeNumber is not null;
-#just checking for changes
-select ordernumber, customerNumber from orders;
-#just checking for changes in GitHub
+show databases;
+use toys_and_models;
+#show tables;
+select count(employeeNumber) from employees; #there are 23 Emp#
+select salesrepemployeenumber, count(customername) from customers group by salesrepemployeenumber;#how many cs per sales rep?
+
+select firstName, lastname as SalesRep, customerName #, orderNumber
+from customers join employees on customers.salesRepEmployeeNumber = employees.employeeNumber
+order by customerName;
+#join orders on customers.customernumber = orders.customerNumber;
+
+select firstName, lastname as SalesRep, customerName , orderDate, orderNumber
+from customers join employees on customers.salesRepEmployeeNumber = employees.employeeNumber
+join orders on customers.customernumber = orders.customerNumber
+order by orderDate;
+
+select orderNumber, sum(quantityOrdered*priceEach) as TotalAmount from orderdetails
+group by orderNumber
+order by orderNumber;
+
 select ordernumber, productCode, quantityordered, priceEach from orderdetails;
 select productCode, quantityinstock, MSRP from products;
-#just checking for changes
+
 select productCode, quantityinstock, MSRP from products;
 
