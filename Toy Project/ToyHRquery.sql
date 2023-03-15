c9d82a36ce621c52aee6ea881770476e9cb768aa
LuPissarra <lpissarra@gmail.com>
2023-03-15 18:01:25 +0000

    I get totals, now need monthly

--- Toy Project/ToyHRquery.sql
+++ Toy Project/ToyHRquery.sql
@@ -1,19 +1,30 @@
-#HR KPI:Monthly, the 2 sellers with the highest turnover
+#HR KPI: Each monthly, the 2 sellers with the highest turnover
 show databases;
 use toys_and_models;
-#show tables;
-select count(employeeNumber) from employees; #there are 23 Emp#
-select salesrepemployeenumber, count(customername) from customers group by salesrepemployeenumber;#how many cs per sales rep?
+show tables;
+#the sellers are found in employee table
+SELECT lastname FROM employees; #23 rows returned
+#sellers link to Total sales: Emp > Cs > Ord > OrdDet
 
-select firstName, lastname as SalesRep, customerName #, orderNumber
-from customers join employees on customers.salesRepEmployeeNumber = employees.employeeNumber
-order by customerName;
-#join orders on customers.customernumber = orders.customerNumber;
+select lastname as Seller, OrderNumber as Ord, quantityOrdered as Qts
+from employees as Emp
+join customers as Cs on Emp.employeeNumber = Cs.salesRepEmployeeNumber #Connect Sellers info to Cs info by the EmpNumb
+join orders as Ords on Cs.customerNumber = Ords.customerNumber #Connect with Ords info by csNumb
+join orderdetails as OrdDets on Ords.orderNumber = OrdDets.orderNumber; #Connect with OrdDets info (qty & prices) by
+#group by lastname
+#order by TotalAmount desc
+#limit 2;
 
-select firstName, lastname as SalesRep, customerName , orderDate, orderNumber
-from customers join employees on customers.salesRepEmployeeNumber = employees.employeeNumber
-join orders on customers.customernumber = orders.customerNumber
-order by orderDate;
+select lastname as Seller /**count(customerName), count(orderDate)**/ , sum(quantityOrdered*priceEach) as TotalAmount 
+from employees as Emp
+join customers as Cs on Emp.employeeNumber = Cs.salesRepEmployeeNumber #Connect Sellers info to Cs info by the EmpNumb
+join orders as Ords on Cs.customerNumber = Ords.customerNumber #Connect with Ords info by csNumb
+join orderdetails as OrdDets on Ords.orderNumber = OrdDets.orderNumber; #Connect with OrdDets info (qty & prices) by
+#group by lastname
+#order by TotalAmount desc
+#limit 2;
+
+select * from orderdetails;
 
 select orderNumber, sum(quantityOrdered*priceEach) as TotalAmount from orderdetails
 group by orderNumber
@@ -23,4 +34,3 @@ select ordernumber, productCode, quantityordered, priceEach from orderdetails;
 select productCode, quantityinstock, MSRP from products;
 
 select productCode, quantityinstock, MSRP from products;
-
